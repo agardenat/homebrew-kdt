c8af29305b132eb2ed0dad36eb48283713c21450
agardenat <50456751+agardenat@users.noreply.github.com>
2026-06-20 22:32:10 +0200

    kdt 1.1.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/kdt.rb
+++ Formula/kdt.rb
@@ -2,9 +2,9 @@
 class MonApp < Formula
   desc "Description de mon app"
   homepage "https://github.com/agardenat/kdt"
-  version "1.1.0"
-  url "https://github.com/agardenat/kdt/releases/download/v1.1.0/kdt-macos-universal.tar.gz"
-  sha256 "eff677f8dc5d6a7a61215710003668937fba73c067293867b41654025c6bc7bb"
+  version "1.1.2"
+  url "https://github.com/agardenat/kdt/releases/download/v1.1.2/kdt-macos-universal.tar.gz"
+  sha256 "47c5663d19775196ebb0682409ce15fc40180d018b1f4d771374a4fb5490acd8"
 
   def install
     bin.install "kdt"
